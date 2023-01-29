4d471ed2a74857358450fcabb47e2b9cdff60b29
Umar Aqeel <112412509+Umar2838@users.noreply.github.com>
2023-01-29 23:37:07 +0500

    added js assignment chapter 4

---
+++ app.js
@@ -0,0 +1,103 @@
+
+
+//<----------------------------------Chapter 02 (Variable & String)--------------------------------------->
+
+// Question 01
+// var username = "";
+// Question 02
+
+// var myName = "Umar Aqeel";
+// console.log(myName);
+
+// Question 03
+//  var message="";
+//  var message ="Hello World"
+// alert(message);
+
+// Question 04
+// var name ="Umar";
+// alert (name);
+// var age ="17 years old";
+// alert(age);
+// var cirtification = "Certified Web & Mobile Aplication";
+// alert(cirtification);
+
+// Question 05
+// var food = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(food);
+
+// Question 06
+// myself = "My email Address is"
+// var email ="umaraqeel@gmail.com";
+// alert(myself+email);
+
+// Question 07
+// trying="I am Trying to learn from the Book "
+// var book ="A smarter\n way to learn JAVASCRIPT";
+// alert(trying+book);
+
+// Question 08 
+// document.write("YAH! I can write HTML content through JavaScript");
+
+// Question 09
+// var design="“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”";
+// alert(design);
+
+
+//<------------------------------------------Chapter no 03 (Variables for Numbetrs)-------------------------->
+
+
+
+// Question no 01
+// var age = "I am 17 years old";
+// alert(age);
+
+//Question no 02
+// var visited;
+// visited="You have visited this website 1st time";
+// alert(visited);
+
+
+//Question no 03
+// var birthYear=2004
+// document.write("My birth year is"+ birthYear );
+// document.write("</br>Data type of my declared variable is number </br>");
+
+//Question 04
+// var visitorsName ="<b>John Dae</b> ";
+// var Producttitle ="<b>T-Shirt(s)</b> ";
+// var Quantity ="ordered<b> 5 </b>"
+// document.write(visitorsName+Quantity+Producttitle);
+// document.write("on XYZ clothing");
+
+
+
+//<------------------------------- Chapter no 04 (Varibles legal & illegal)------------------------------>
+
+
+
+//Question no 01
+// var fruit="Mango,";
+// var vegetable="Carrot,";
+// var dryFruit="Almonds";
+// document.write("I like to eat "+fruit,vegetable,dryFruit );
+
+//Question no 02
+//legal Varibles
+// var _car="honda";
+// var City="Karachi";
+// var $currency="PKR";
+// var country_name="Pakistan";
+// var saylaniWelfareMassITProgram="SMIT";
+
+//Illegal Variables
+// var 2car="honda";
+// var class="12 year";//(keyword of javascript)
+// var @email="example@gmail.com";
+// var country name="Pakistan";
+// var number#="03676266872";
+
+//Question no 03
+// document.write("<h1>Rules for naming JS variables</h1>");
+// document.write("Variable names can only contain ,numbers,$ and_.For Example:$my_1stVariable.</br>Variables must begin with a letter, $ or _. For Example:$name,_name,name.</br>Variable name are case sensitive.<br>Variable name should not be JS keywords.");
+
